f62df9b732e88ad75dba38f70361e22b966445f1
Ben Milton <ben.c.milton@gmail.com>
2021-02-22 09:38:25 -0500

    add new projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,26 +1,43 @@
-import { FC } from 'react';
+import { FC, ReactElement } from 'react';
 import { Parallax } from 'react-parallax';
 import classNames from 'classnames';
 
 import styles from './Project.module.css';
 
 type Props = {
-    className: string;
+    color: string;
+    fontColor?: string;
     title: string;
     subtitle: string;
     imageUrl: string;
     insight: string;
-    outcome: string;
+    action: string;
+    outcome?: string | ReactElement;
+    recommendation?: string | ReactElement;
 };
 
-const Project: FC<Props> = ({ className, title, subtitle, imageUrl, insight, outcome }) => (
-    <Parallax blur={10} className={className} strength={200}>
-        <div className={styles.container}>
-            <div className={styles.title}>
-                <h3>{title}</h3>
-                <p>{subtitle}</p>
+const Project: FC<Props> = ({
+    color,
+    fontColor,
+    title,
+    subtitle,
+    imageUrl,
+    insight,
+    action,
+    outcome,
+    recommendation,
+}) => (
+    <Parallax blur={10} className={styles[color]} strength={200}>
+        <div
+            className={classNames(styles.container, {
+                [styles.fontColorBlack]: fontColor === 'black',
+            })}
+        >
+            <div className={styles.header}>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.subtitle}>{subtitle}</div>
             </div>
-            <div className={styles.stuff}>
+            <div className={styles.body}>
                 <div className={styles.imageContainer}>
                     <img src={imageUrl} width={580} height={427} />
                 </div>
@@ -30,9 +47,21 @@ const Project: FC<Props> = ({ className, title, subtitle, imageUrl, insight, out
                         <span>{insight}</span>
                     </div>
                     <div className={styles.insights}>
-                        <span className={styles.bold}>Outcome:</span>
-                        <span>{outcome}</span>
+                        <span className={styles.bold}>Action:</span>
+                        <span>{action}</span>
                     </div>
+                    {outcome && (
+                        <div className={styles.insights}>
+                            <span className={styles.bold}>Outcome:</span>
+                            <span>{outcome}</span>
+                        </div>
+                    )}
+                    {recommendation && (
+                        <div className={styles.insights}>
+                            <span className={styles.bold}>Recommendation:</span>
+                            <span>{recommendation}</span>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,26 +1,114 @@
 import { FC } from 'react';
 
 import Project from './Project';
+import Link from './Link';
 import styles from './Projects.module.css';
 
 const PROJECTS = [
     {
-        title: 'SNOWE PENTHOUSE WEST',
+        title: 'DIFFERENTIATING A DTC BRAND',
         subtitle:
-            'An immersive retail experience designed to meet consumers where they are - in their homes.',
-        className: styles.yellow,
-        insight: 'Consumers',
-        outcome: 'We win!',
+            'Using Scott Galloway’s (NYU Stern, Section4) strategy frameworks, developed recommendations for an ecommerce company.',
+        color: 'blue',
+        insight:
+            'With Covid-19 accelerating a focus on home and consumer confidence growing online, Snowe can leverage their whole-home offering to establish category dominance.',
+        action:
+            'Conducted a brand audit, created a brand identity model, and pulled market research and consumer insights to craft actionable recommendations.',
+        imageUrl: '/70-Pine_Living-Room-1280w.webp',
+        recommendation: (
+            <span>
+                Snowe can win by building community, adopting social commerce, and developing a
+                subscription model, owned and in partnership, that creates a recurring revenue
+                stream to grow brand equity and business bottomline.
+                <p>
+                    <i>Case Study</i>
+                </p>
+            </span>
+        ),
+    },
+    {
+        title: 'LAUNCHING A GROCERY CPG',
+        subtitle:
+            'Bringing a brand to life from brand foundation to go to market strategy and future growth plan.',
+        color: 'yellow',
+        fontColor: 'black',
+        insight:
+            'Consumers have adopted buying food online and DTC, and are keen to discover new products.',
+        action:
+            'Using market research, consumer insights, and cultural trends, created a brand identity system and product strategy to bring this idea to fruition.',
+        outcome: (
+            <span>
+                Developed an elevated CPG brand targeted to a niche ethnic demographic. Along with a
+                product line of high-quality foods, took a holistic approach to create a brand that
+                consumers can learn and grow with.
+                <p>
+                    <i>In Progress</i>
+                </p>
+            </span>
+        ),
+        imageUrl: '/CPG_Grocery_GTM.webp',
+    },
+    {
+        title: 'INNOVATING BRICK & MORTAR',
+        subtitle:
+            'An immersive retail experience delivered within a luxury residential high-rise in Manhattan.',
+        color: 'green',
+        insight:
+            'Partnering with real estate developers to stage units as shoppable showrooms, Snowe could reach consumers as close to home as possible, making it more accessible than ever to see and shop for their apartment.',
+        action:
+            'Designed, installed and launched an aspirational Snowe home. Negotiated key brand partnerships whose involvement increased social visibility. ',
+        outcome: (
+            <span>
+                Launched to industry insiders, held exclusive shopping appointments, and generated
+                interest from real estate developers to replicate the idea in other markets.{' '}
+                <Link href="https://businessofhome.com/articles/snowe-and-industry-west-want-to-meet-clients-where-they-are-at-home">
+                    Press
+                </Link>{' '}
+                & <Link href="https://www.snowepenthousewest.com/">Microsite</Link>.
+                <p>
+                    <i>Executed</i>
+                </p>
+            </span>
+        ),
         imageUrl: '/70-Pine_Living-Room-1280w.webp',
     },
     {
-        title: "FLAVITA'S",
+        title: 'TARGETING B2B2C',
         subtitle:
-            'An immersive retail experience designed to meet consumers where they are - in their homes. ',
-        className: styles.green,
-        insight: 'Consumers',
-        outcome: 'Hot Sauceee!',
-        imageUrl: '/CPG_Grocery_GTM.webp',
+            'Reimagined Snowe’s Trade program to be a competitive and compelling offering for design professionals.',
+        color: 'black',
+        insight: 'Design professionals were an untapped',
+        action:
+            'Create an aspirational Snowe home, from design to installation, while negotiating key brand partnerships to increase outreach and visibility.',
+        outcome: (
+            <span>
+                Saw a 140% increase in YoY traffic and 2x average time on site.
+                <p>
+                    <i>Executed</i>
+                </p>
+            </span>
+        ),
+        imageUrl: '/70-Pine_Living-Room-1280w.webp',
+    },
+    {
+        title: 'BUILDING AN OMNICHANNEL STRATEGY',
+        subtitle:
+            'Developed the marketing strategy for a luxury wedding vendor, with a focus on digital.',
+        color: 'red',
+        fontColor: 'black',
+        insight:
+            'By partnering with real estate developers to a stage units as a shoppable showroom, Snowe could reach consumers as close to home as possible, making it more accessible than ever to see and shop for their apartment.',
+        action:
+            'Create an aspirational Snowe home, from design to installation, while negotiating key brand partnerships to increase outreach and visibility.',
+        outcome: (
+            <span>
+                525% growth in Instagram followers over 3 years
+                <p>
+                    <i>Executed</i>
+                </p>
+            </span>
+        ),
+        imageUrl: '/70-Pine_Living-Room-1280w.webp',
     },
 ];
 
