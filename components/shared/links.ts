58bf3144b96e650a08a411d959dde6dd0c54d5bd
Ben Milton <ben.c.milton@gmail.com>
2021-03-02 12:56:35 -0500

    update resume url

--- components/shared/links.ts
+++ components/shared/links.ts
@@ -1,7 +1,8 @@
-export const emailUrl = 'mailto:holazsv@gmail.com?subject=¡Hola Zaira!';
+export const emailUrl =
+    'mailto:holazsv@gmail.com?subject=¡Hola Zaira!';
 
 export const linkedInUrl = 'https://www.linkedin.com/in/z-s-v/';
 
 export const instagramUrl = 'https://www.instagram.com/zairavallejo/';
 
-export const resumeUrl = 'https://drive.google.com/file/d/1KTP-mhFr8mewMeoYcrcE0gPhA_d1ktwK/view';
+export const resumeUrl = 'http://bit.ly/zsv_resume';
