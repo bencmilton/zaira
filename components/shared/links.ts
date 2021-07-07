8cb4b05208787e22146219d11f5aca284508ae4e
Ben Milton <ben.c.milton@gmail.com>
2021-07-07 10:58:04 -0400

    update resume link

--- components/shared/links.ts
+++ components/shared/links.ts
@@ -5,4 +5,4 @@ export const linkedInUrl = 'https://www.linkedin.com/in/z-s-v/';
 
 export const instagramUrl = 'https://www.instagram.com/zairavallejo/';
 
-export const resumeUrl = 'http://bit.ly/zsv_resume';
+export const resumeUrl = 'https://drive.google.com/file/d/1IZ_B561iLf-3fLIf3CJ5C6pI1iiHQkH6/view?usp=sharing';
