737a0a2607025054733f662f4f2d50c304eab321
Ben Milton <ben.milton@shopify.com>
2024-02-11 14:27:16 -0500

    Update link URLs

--- components/shared/links.ts
+++ components/shared/links.ts
@@ -3,6 +3,7 @@ export const emailUrl =
 
 export const linkedInUrl = 'https://www.linkedin.com/in/z-s-v/';
 
-export const instagramUrl = 'https://www.instagram.com/zairavallejo/';
+export const instagramUrl = 'https://www.instagram.com/zsvszsvsz/';
 
-export const resumeUrl = 'https://bit.ly/3gxJsQQ';
+export const resumeUrl =
+    'https://drive.google.com/file/d/1Bd8ZWcP7aZffNoI9vLpKKKMchvBe9HYR/view?usp=sharing';
