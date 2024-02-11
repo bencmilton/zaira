ad6c463b9aea6f08cc76cc4f19911be0feff8091
Ben Milton <ben.milton@shopify.com>
2024-02-11 14:41:01 -0500

    Update resume URL

--- components/shared/links.ts
+++ components/shared/links.ts
@@ -5,5 +5,4 @@ export const linkedInUrl = 'https://www.linkedin.com/in/z-s-v/';
 
 export const instagramUrl = 'https://www.instagram.com/zsvszsvsz/';
 
-export const resumeUrl =
-    'https://drive.google.com/file/d/1Bd8ZWcP7aZffNoI9vLpKKKMchvBe9HYR/view?usp=sharing';
+export const resumeUrl = 'https://bit.ly/zsvresume';
